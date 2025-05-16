2a3734fe3bfc1b5dfb3c7d0b2a9379cfd53a4a03
unknown <jojeq998@gmail.com>
2025-05-16 11:57:03 +0200

    first commit

---
+++ db_connect.php
@@ -0,0 +1,17 @@
+<?php 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+
+require_once 'config.php';
+
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+
+//echo $mysqli->connect_errno;
+
+if($mysqli->connect_error){
+    die("Blad polaczenia: " . $mysqli->connect_error);
+}
